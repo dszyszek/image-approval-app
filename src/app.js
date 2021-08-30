d6551100d9e843f2caadfe372e159f67406667ca
Damian Szyszka <dszys@softserveinc.com>
2021-08-30 04:03:10 +0200

    Extend app.js

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+
+import AppRouter from "./router";
+
+ReactDOM.render(<AppRouter />, document.querySelector("#app"));
