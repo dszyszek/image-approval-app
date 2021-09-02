81aefbff84ba7eaf11f01ba1c3614f71476db290
Damian Szyszka <dszys@softserveinc.com>
2021-09-02 22:43:53 +0200

    Rename ApprovePicturesThumbnail to LabeledThumbnailSlider

---
+++ src/molecules/LabeledThumbnailSlider/LabeledThumbnailSlider.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import defaultStyles, { wrapperFactory, defaultImageStyles } from "./styles";
+import { Text, Image } from "../../atoms";
+import { PicturesCarousel } from "..";
+import { merge } from "../../utils";
+
+const LabeledThumbnailSlider = ({ styles, approvedPicturesTitle, images }) => {
+  const {
+    wrapper: wrapperStyles,
+    text: { wrapper: textWrapperStyles, text: textStyles },
+    picturesCarousel: picturesCarouselStyles,
+    picturesCarouselWrapper: picturesCarouselWrapperStyles,
+  } = merge(defaultStyles, styles);
+  const Wrapper = wrapperFactory(wrapperStyles);
+  const CarouselWrapper = wrapperFactory(picturesCarouselWrapperStyles);
+  const TextWrapper = wrapperFactory(textWrapperStyles);
+  const carousel = images.length ? (
+    <PicturesCarousel styles={picturesCarouselStyles} items={images} />
+  ) : (
+    <Image styles={defaultImageStyles} />
+  );
+
+  return (
+    <Wrapper>
+      <TextWrapper>
+        <Text styles={textStyles}>{approvedPicturesTitle}</Text>
+      </TextWrapper>
+      <CarouselWrapper>{carousel}</CarouselWrapper>
+    </Wrapper>
+  );
+};
+
+LabeledThumbnailSlider.defaultProps = {
+  styles: {},
+  approvedPicturesTitle: "",
+  images: [],
+};
+LabeledThumbnailSlider.propTypes = {
+  styles: PropTypes.shape({
+    wrapper: PropTypes.objectOf(PropTypes.any),
+    text: PropTypes.objectOf(PropTypes.any),
+    picturesCarousel: PropTypes.objectOf(PropTypes.string),
+    picturesCarouselWrapper: PropTypes.objectOf(PropTypes.string),
+  }),
+  approvedPicturesTitle: PropTypes.string,
+  images: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)),
+};
+
+export default LabeledThumbnailSlider;
---
+++ src/molecules/LabeledThumbnailSlider/index.js
@@ -0,0 +1,1 @@
+export { default as LabeledThumbnailSlider } from "./LabeledThumbnailSlider";
