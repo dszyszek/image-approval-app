c71f593d8ed652f15e2d46ee1bf79ea77d84e684
Damian Szyszka <dszys@softserveinc.com>
2021-09-03 02:28:12 +0200

    Add more mocked values

--- src/pages/PicturePage/PicturePage.js
+++ src/pages/PicturePage/PicturePage.js
@@ -5,31 +5,50 @@ import { UNSPLASH_BASE } from "../../constants";
 import { PictureTemplate } from "../../templates";
 
 const placeholderImages = [
-  // [
-  //   "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
-  //   "placeholder1",
-  // ],
-  // [
-  //   "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
-  //   "placeholder2",
-  // ],
-  // [
-  //   "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
-  //   "placeholder3",
-  // ],
-  // [
-  //   "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
-  //   "placeholder4",
-  // ],
-  // [
-  //   "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
-  //   "placeholder5",
-  // ],
+  [
+    "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
+    "placeholder1",
+  ],
+  [
+    "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
+    "placeholder2",
+  ],
+  [
+    "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
+    "placeholder3",
+  ],
+  [
+    "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
+    "placeholder4",
+  ],
+  [
+    "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
+    "placeholder5",
+  ],
 ];
+
+const placeholderMainImage = {
+  src: "https://images.unsplash.com/1/type-away.jpg?q=80&fm=jpg&w=200&fit=max",
+  alt: "placeholder1",
+};
+
+const placeholderThumbnailGalleryLabel = "approved images (0)";
+const placeholderHeaderText = "image approval application";
+
+const onMainPictureClickPlaceholder = () =>
+  console.log("onMainPictureClick placeholder");
 
 export default () => {
   const api = new API(UNSPLASH_BASE);
-  const data = { headerText: "some text", carouselImages: placeholderImages };
+  const data = {
+    headerText: placeholderHeaderText,
+    carouselImages: placeholderImages,
+    mainPicture: placeholderMainImage,
+    thumbnailGalleryLabel: placeholderThumbnailGalleryLabel,
+  };
+  const control = {
+    onMainPictureClick: onMainPictureClickPlaceholder,
+  };
 
-  return <PictureTemplate data={data} />;
+  return <PictureTemplate control={control} data={data} />;
 };
