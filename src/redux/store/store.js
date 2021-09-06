5e40ab4f6e5851fe4a78101093e6da8bc755507a
Damian Szyszka <dszys@softserveinc.com>
2021-09-06 02:12:51 +0200

    Add redux-persist

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,18 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { rootReducer } from "../reducers";
 
 const middleware = [thunk];
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -10,5 +20,6 @@ const composeEnhancers =
     : compose;
 
 const enhancer = composeEnhancers(applyMiddleware(...middleware));
-const store = createStore(rootReducer, enhancer);
+const store = createStore(persistedReducer, enhancer);
+const persistor = persistStore(store);
 export default store;
